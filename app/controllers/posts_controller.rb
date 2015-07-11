928dd4ac0020fd97a5c5dd2f7c261e407dae901c
M1lena <stoevami@gmail.com>
2015-07-11 13:33:21 +0200

    Can add posts. All tests pass.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,17 @@
 class PostsController < ApplicationController
+  def index
+    @post = Post.all
+  end
+
+  def new
+  end
+
+  def create
+    Post.create(post_params)
+    redirect_to '/'
+  end
+
+  def post_params
+    params.require(:post).permit(:caption)
+  end
 end
--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -8,4 +8,27 @@ feature 'posts' do
       expect(page).to have_link 'add a post'
     end
   end
+
+  context 'posts have been added' do
+    before do
+      Post.create(caption: 'sunny Saturday')
+    end
+
+    scenario 'display posts' do
+      visit '/posts'
+      expect(page).to have_content('sunny Saturday')
+      expect(page).not_to have_content('No posts yet')
+    end
+  end
+
+  context 'creating posts' do
+    scenario 'prompts user to fill out a form, then displays the new post' do
+      visit '/posts'
+      click_link 'add a post'
+      fill_in 'Caption', with: 'sunny Saturday'
+      click_button 'Create Post'
+      expect(page).to have_content 'sunny Saturday'
+      expect(current_path).to eq '/'
+    end
+  end
 end
